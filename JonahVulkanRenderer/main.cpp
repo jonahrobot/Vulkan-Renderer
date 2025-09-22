7ca4976b1a1823c8d1bdc76ba192e9a113de4209
Jonah Ryan <jonahrobot@gmail.com>
2025-09-21 19:30:39 -0800

    Create Swapchain
    Refactor is next!

--- JonahVulkanRenderer/main.cpp
+++ JonahVulkanRenderer/main.cpp
@@ -1,6 +1,5 @@
-#define SDL_MAIN_HANDLED
-
 #define VK_USE_PLATFORM_WIN32_KHR
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -11,8 +10,9 @@
 #include <string.h>
 #include <set>
 #include <optional>
+#include <limits>
+#include <algorithm>
 #include <iostream>
-
 
 #define WIDTH 500
 #define HEIGHT 500
@@ -312,6 +312,111 @@ int CreateVulkanInstance(VkInstance* VulkanInstance, GLFWwindow* window){
 	return 0;
 }
 
+// Preferred: SRGB 8 bit
+VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+	
+	for (const auto& availableFormat : availableFormats) {
+		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR){
+			return availableFormat;
+		}
+	}
+
+	return availableFormats[0];
+}
+
+// Preferred: Mailbox present mode
+VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+	for (const auto& availablePresentMode : availablePresentModes) {
+		if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+			return availablePresentMode;
+		}
+	}
+
+	return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+// Sets swapchain size to window size
+VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities, GLFWwindow* window) {
+	if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+		// Usual case: Swapchain size == window resolution
+		return capabilities.currentExtent;
+	}
+	else {
+		// Window manager wants us to pick scale of swapchain.
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+
+		VkExtent2D actualExtent = {
+			static_cast<uint32_t>(width),
+			static_cast<uint32_t>(height)
+		};
+
+		actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+		return actualExtent;
+	}
+}
+
+VkSurfaceFormatKHR surfaceFormat;
+VkExtent2D extent;
+
+VkSwapchainKHR createSwapChain(VkPhysicalDevice device, VkSurfaceKHR surface, GLFWwindow* window, VkDevice logicDevice) {
+
+	SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device, surface);
+
+	surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+	VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+	extent = chooseSwapExtent(swapChainSupport.capabilities, window);
+	uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+	// maxImageCount of 0 means no max image count.
+	if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) {
+		imageCount = swapChainSupport.capabilities.maxImageCount;
+	}
+
+	// Define Swap Chain Creation Struct
+	VkSwapchainCreateInfoKHR createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	createInfo.surface = surface;
+	createInfo.minImageCount = imageCount;
+	createInfo.imageFormat = surfaceFormat.format;
+	createInfo.imageColorSpace = surfaceFormat.colorSpace;
+	createInfo.imageExtent = extent;
+	createInfo.imageArrayLayers = 1; // Always 1 unless stereoscopic 3D.
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+	QueueFamilyIndices indices = findQueueFamilies(device, surface);
+	uint32_t queueFamilyIndices[] = { indices.graphicsFamily.value(), indices.presentFamily.value() };
+
+	if (indices.graphicsFamily != indices.presentFamily) {
+		// Concurrent handling of queues when graphics and present logic
+		// on different queues.
+		createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+		createInfo.queueFamilyIndexCount = 2;
+		createInfo.pQueueFamilyIndices = queueFamilyIndices;
+	}
+	else {
+		// Most common mode, graphic and present on same queue family.
+		createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	}
+
+	// Modify if we want to apply transformations to images on swapchain
+	createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+	createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	createInfo.presentMode = presentMode;
+	createInfo.clipped = VK_TRUE; // Don't render pixels obsured by another window.
+	createInfo.oldSwapchain = VK_NULL_HANDLE; // Must use when swapchain becomes invalid (window resize)
+
+	VkSwapchainKHR swapChain;
+
+	if (vkCreateSwapchainKHR(logicDevice, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create swap chain!");
+	}
+
+	return swapChain;
+}
+
 int main() {
 
 	// Init GLFW
@@ -352,6 +457,16 @@ int main() {
 	// Logical device selection -> Specify which features and queue families
 	VkDevice VkLogicDevice = CreateLogicalDevice(VkInstance, VkPhyDevice, VkSurface);
 	
+	// Create SwapChain
+	VkSwapchainKHR VkSwapChain = createSwapChain(VkPhyDevice, VkSurface, window, VkLogicDevice);
+
+	// Get Images
+	std::vector<VkImage> swapChainImages;
+	uint32_t imageCount = 0;
+	vkGetSwapchainImagesKHR(VkLogicDevice, VkSwapChain, &imageCount, nullptr);
+	swapChainImages.resize(imageCount);
+	vkGetSwapchainImagesKHR(VkLogicDevice, VkSwapChain, &imageCount, swapChainImages.data());
+
 	// Get queue references
 	QueueFamilyIndices indices = findQueueFamilies(VkPhyDevice, VkSurface);
 
@@ -363,11 +478,11 @@ int main() {
 
 	// Main Application Loop
 	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-		
+		glfwPollEvents();	
 	}
 
 	// Clean up
+	vkDestroySwapchainKHR(VkLogicDevice, VkSwapChain, nullptr);
 	vkDestroyDevice(VkLogicDevice, nullptr);
 	vkDestroySurfaceKHR(VkInstance, VkSurface, nullptr);
 	vkDestroyInstance(VkInstance, nullptr); // Cleanup instance LAST in Vulkan Cleanup
